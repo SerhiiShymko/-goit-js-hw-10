5e91f0e43a5d38350b862482c66a71c01124fbdc
SerhiiShymko <shimko456@gmail.com>
2023-03-27 16:54:24 +0300

    01

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '../src/css/styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryMarkUp = document.querySelector('#search-form');
+const btnSearch = document.querySelector('.search');
+const btnLoad = document.querySelector('.load-more');
