d3581fe8d88a781926ee0afda47fb30cf58fca5e
SerhiiShymko <shimko456@gmail.com>
2023-03-28 21:58:20 +0300

    01

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import '../src/css/styles.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getImages } from './js/apiClient.js';
+import throttle from 'lodash.throttle';
+// import templates from './templates/template-card.hbs';
 
 const galleryMarkUp = document.querySelector('#search-form');
 // const btnSearch = document.querySelector('.search');
 // const btnLoad = document.querySelector('.load-more');
 
-// let searchState = '';
-// let pageState = 1;
+let searchState = '';
+let currentPage = 1;
+let currentHits = 0;
 
 document.querySelector('.load-more').addEventListener('click', () => {
   const btnLoad = document.querySelector('.load-more').dataset.page;
   const btnSearch = document.querySelector('.search').value;
-  //   fetchNews(search, page);
+  getImages(search, page);
 });
 
 const fetchImages = (search, page) =>
@@ -43,8 +46,8 @@ const updateLoadButton = currentPage => {
   btn.dataset.page = Number(currentPage) + 1;
 };
 
-// const lightbox = new SimpleLightbox('getImages', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   captionPosition: 'bottom',
-// });
+const lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/apiClient.js
+++ src/js/apiClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const getImages = (search, page) => {
+  const BASE_URL = `https://pixabay.com/api/`;
   const params = new URLSearchParams({
     q: search,
     apiKey: process.env.PIXABAY_API_KEY,
@@ -10,11 +11,11 @@ export const getImages = (search, page) => {
     page,
     per_page: 40,
   });
-  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
+  return fetch(`${BASE_URL}?${params}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
   });
+  // return await axios.get(`${BASE_URL}${params}`).then(response => response.data);
 };
-
